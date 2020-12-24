1ee9ecac0a1e419bb6b83b02b852293bc59dc371
Ka3achka <ira.breusova@gmail.com>
2020-12-25 03:46:37 +0700

    + navgraph

--- app/src/main/java/com/example/laba4/signin/SignInActivity.kt
+++ app/src/main/java/com/example/laba4/signin/SignInActivity.kt
@@ -3,33 +3,52 @@ package com.example.laba4.signin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.laba4.R
 import com.example.laba4.databinding.ActivitySignInBinding
 import com.example.laba4.singup.SignUpActivity
 
-class SignInActivity : AppCompatActivity(), SignInView {
+class SignInActivity : Fragment(), SignInView {
+
     private var _binding:ActivitySignInBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val signInViewModel = SignInViewModel(this)
-        _binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+        _binding = DataBindingUtil.setContentView(requireActivity(), R.layout.activity_sign_in)
         binding.signInViewModel = signInViewModel
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.activity_sign_in,
+            container, false)
+
+        return view
+    }
+
+
 
     override fun onSignInSuccess(email: String, password: String) {
         //go to player's info
     }
 
     override fun goToSignUp() {
-        val intent = Intent(this, SignUpActivity::class.java)
-        startActivity(intent)
+        //val intent = Intent(this, SignUpActivity::class.java)
+        //startActivity(intent)
     }
 
     override fun onSignInError(message: String) {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+        Toast.makeText(requireContext(),message,Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/laba4/singup/SignUpActivity.kt
+++ app/src/main/java/com/example/laba4/singup/SignUpActivity.kt
@@ -3,10 +3,22 @@ package com.example.laba4.singup
 import androidx.appcompat.app.AppCompatActivity
 import com.example.laba4.R
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class SignUpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+class SignUpActivity : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.activity_sign_up,
+            container, false)
+
+        return view
     }
 }
